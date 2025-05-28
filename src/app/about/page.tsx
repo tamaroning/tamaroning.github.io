6dddbb599175ae46cbe80727395bc65af637e17a
Raiki Tamura <tamaron1203@gmail.com>
2025-05-28 12:40:08 +0900

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Text, Button } from "@mantine/core";
+
+export default function About() {
+	return (
+		<div>
+			<Text>tamaron</Text>
+			<Text>tamaronは、WebGLを使用して地球を3Dで表示するためのライブラリです。</Text>
+			<Text>このライブラリは、地球の表面をリアルタイムでレンダリングし、ユーザーが地球を回転させたりズームイン・アウトしたりできるようにします。</Text>
+			<Text>tamaronは、地球の地形や大気の効果をリアルに再現し、ユーザーに没入感のある体験を提供します。</Text>
+		</div>
+	);
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,35 @@
+import { Anchor, Box, Burger, Container, Group } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+const mainLinks = [
+	{ link: '/about', label: 'About' },
+];
+
+export function MyHeader() {
+	const [opened, { toggle }] = useDisclosure(false);
+
+	const mainItems = mainLinks.map((item) => (
+		<Anchor<'a'>
+			href={item.link}
+			key={item.label}
+		>
+			{item.label}
+		</Anchor>
+	));
+
+	return (
+		<Container style={{ paddingTop: 10, paddingBottom: 10 }} size="lg">
+			<Box visibleFrom="sm">
+				<Group gap={20} justify="left" >
+					{mainItems}
+				</Group>
+			</Box>
+			<Burger
+				opened={opened}
+				onClick={toggle}
+				size="sm"
+				hiddenFrom="sm"
+			/>
+		</Container>
+	);
+}
