4953f4e11312442d1186d82c8c0d3a6a1771e948
Raiki Tamura <tamaron1203@gmail.com>
2025-08-30 16:17:49 +0900

    ok

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 export default function About() {
   return (
     <div style={{ margin: 20 }}>
+
       <Text fw={700} size="lg">
         tamaron
       </Text>
@@ -21,12 +22,32 @@ export default function About() {
 
       <Space h="md" />
 
+      <Text>
+        I am a master student at Kyoto University.
+        I love learning new things and applying them to real-world problems.
+      </Text>
+
+      <Space h="md" />
+
+      <Text fw={700} size="lg">
+        Skills
+      </Text>
+      <List>
+        <List.Item>Experienced: C, C++, Rust, WebAssembly</List.Item>
+        <List.Item>
+          Intermediate: React, TypeScript, JavaScript, Python
+        </List.Item>
+        <List.Item>Tools: Git, GCP, Firebase, Linux, Docker</List.Item>
+      </List>
+
+      <Space h="md" />
+
       <Text fw={700} size="lg">
         Research Interests
       </Text>
       <List>
-        <List.Item>Compilers, JIT compilers, WebAssembly</List.Item>
-        <List.Item>Program analysis, Rust</List.Item>
+        <List.Item>Compilers, Interpreters, Program analyses</List.Item>
+        <List.Item>WebAssembly, Rust</List.Item>
       </List>
 
       <Space h="md" />
@@ -51,10 +72,10 @@ export default function About() {
         Work Experience
       </Text>
       <List>
-        <List.Item>Aug 2025 - Present: Woven by Toyota. (Intern)</List.Item>
-        <List.Item>Dec 2023 - Present: estie, inc. (Part-time)</List.Item>
-        <List.Item>Aug 2023 - Sep 2024: Sony. (Intern)</List.Item>
-        <List.Item>Aug 2022 - Sep 2025: NTT R&D. (Intern)</List.Item>
+        <List.Item>Aug 2025 - Present: Woven by Toyota (Intern).</List.Item>
+        <List.Item>Dec 2023 - Present: estie, inc (Part-time).</List.Item>
+        <List.Item>Aug 2023 - Sep 2024: Sony (Intern).</List.Item>
+        <List.Item>Aug 2022 - Sep 2025: NTT R&D (Intern).</List.Item>
       </List>
 
       <Space h="md" />
@@ -81,12 +102,35 @@ export default function About() {
           2nd Place.
         </List.Item>
         <List.Item>
-          wanco: ライブマイグレーションをサポートするWebAssemblyコンパイラ,
+          wanco: ライブマイグレーションをサポートするWebAssemblyコンパイラ.
           田村来希, 小谷大祐, 松本直樹, 岡部寿男. <br />
           <Link href="https://sigos.ipsj.or.jp/event/comsys2024/">
             情報処理学会 第36回コンピュータシステム・シンポジウム (ComSys 2024)
           </Link>
-          , 優秀若手発表賞.
+          . 優秀若手発表賞.
+        </List.Item>
+      </List>
+
+      <Space h="md" />
+
+      <Text fw={700} size="lg">
+        Presentations
+      </Text>
+      <List>
+        <List.Item>
+          2025 AsiaLLVM. Wanco: WebAssembly AOT Compiler That Supports Live
+          Migration
+          <Link
+            href="https://docs.google.com/presentation/d/1fbAiPh-o_3Xg10fX0Zf-ngISpc_8olzNYTeVDzwqWW8/edit?usp=sharing"
+          >(slides)</Link>
+          .
+        </List.Item>
+        <List.Item>
+          FOSDEM 2024. Unicode Support for GCC Rust
+          <Link
+            href="https://docs.google.com/presentation/d/1mdxOBGrBCozhx3hY729tWGh33bqqgYO0xddpK5ql0qY/edit?usp=drive_link"
+          >(slides)</Link>
+          .
         </List.Item>
       </List>
 
@@ -131,18 +175,12 @@ export default function About() {
       <Space h="md" />
 
       <Text fw={700} size="lg">
-        Presentations
+        Awards
       </Text>
       <List>
-        <List.Item>
-          2025 AsiaLLVM. Wanco: WebAssembly AOT Compiler That Supports Live
-          Migration.
-          <Link href="TODO: link">(slides)</Link>
-        </List.Item>
-        <List.Item>
-          FOSDEM 2024. Unicode Support for GCC Rust.
-          <Link href="TODO: link">(slides)</Link>
-        </List.Item>
+        <List.Item>IPSJ Computer Science Research Award for Young Scientists (2025)</List.Item>
+        <List.Item>アプリ甲子園 2018 優勝</List.Item>
+        <List.Item>Unityインターハイ 2018 ゴールドアワード</List.Item>
       </List>
 
       <Space h="md" />
@@ -152,84 +190,32 @@ export default function About() {
       </Text>
       <List>
         <List.Item>
-          2025
-          <List>
-            <List.Item>
-              Kyoto University, Faculty of Engineering, Department of
-              Informatics, Operating Systems. TA
-            </List.Item>
-            <List.Item>
-              Kyoto University, Faculty of Engineering, Department of
-              Informatics, Computer Architecture. TA
-            </List.Item>
-          </List>
+          2025年度未踏IT人材発掘・育成事業
         </List.Item>
         <List.Item>
-          2024
-          <List>
-            <List.Item>セキュリティ・ミニキャンプ熊本2024 チューター</List.Item>
-          </List>
+          Kyoto University, Faculty of Engineering, Department of
+          Informatics, Operating Systems. TA
         </List.Item>
         <List.Item>
-          2023
-          <List>
-            <List.Item>
-              Google Summer of Code 2023. (Organization: GCC)
-            </List.Item>
-            <List.Item>
-              Global Cybersecurity Camp 2023 Singapore. Tutor
-            </List.Item>
-            <List.Item>セキュリティ・ミニキャンプ宮崎2023 チューター</List.Item>
-          </List>
+          Kyoto University, Faculty of Engineering, Department of
+          Informatics, Computer Architecture. TA
+        </List.Item>
+        <List.Item>セキュリティ・ミニキャンプ熊本2024 チューター</List.Item>
+        <List.Item>
+          Google Summer of Code 2023. (Organization: GCC)
         </List.Item>
         <List.Item>
-          2022
-          <List>
-            <List.Item>
-              セキュリティ・キャンプ全国大会 2022. チューター
-            </List.Item>
-          </List>
+          Global Cybersecurity Camp 2023 Singapore. Tutor
+        </List.Item>
+        <List.Item>セキュリティ・ミニキャンプ宮崎2023 チューター</List.Item>
+        <List.Item>
+          セキュリティ・キャンプ全国大会 2022. チューター
         </List.Item>
         <List.Item>
-          2021
-          <List>
-            <List.Item>
-              セキュリティ・キャンプ全国大会 2021 脅威解析トラックコース 修了
-            </List.Item>
-          </List>
+          セキュリティ・キャンプ全国大会 2021 脅威解析トラックコース 修了
         </List.Item>
-        <List.Item>
-          2020
-          <List>
-            <List.Item>虚無</List.Item>
-          </List>
-        </List.Item>
-        <List.Item>
-          2019
-          <List>
-            <List.Item>第8回 科学の甲子園 全国大会 埼玉県代表</List.Item>
-          </List>
-        </List.Item>
-        <List.Item>
-          2018
-          <List>
-            <List.Item>SecHack365 開発駆動コース 修了</List.Item>
-            <List.Item>アプリ甲子園 2018 優勝</List.Item>
-          </List>
-        </List.Item>
-      </List>
-
-      <Space h="md" />
-
-      <Text fw={700} size="lg">
-        Skills
-      </Text>
-      <List>
-        <List.Item>Experienced: C, C++, Rust, WebAssembly</List.Item>
-        <List.Item>
-          Intermediate: React, TypeScript, JavaScript, Python
-        </List.Item>
-        <List.Item>Tools: Git, GCP, Firebase, Linux, Docker</List.Item>
+        <List.Item>第8回 科学の甲子園 全国大会 埼玉県代表</List.Item>
+        <List.Item>SecHack365 開発駆動コース 修了</List.Item>
       </List>
     </div>
   );
