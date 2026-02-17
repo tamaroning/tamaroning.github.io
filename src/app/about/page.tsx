76b6cbe4cf3fe19e31d61f1a14e0f842e580de11
Raiki Tamura <tamaron1203@gmail.com>
2026-02-17 16:14:24 +0900

    update about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,49 +6,28 @@ import Link from "next/link";
 
 export default function About() {
   return (
-    <div style={{ margin: 20 }}>
-
-      <Text fw={700} size="lg">
-        tamaron
-      </Text>
-
-      <Image
-        src="/assets/icon.jpg"
-        alt="icon"
-        width={300}
-        height={300}
-        style={{ borderRadius: "50%" }}
-      />
-
-      <Space h="md" />
+    <div style={{ maxWidth: 560, margin: 20 }}>
+      <div style={{ display: "flex", alignItems: "flex-start", gap: 24 }}>
+        <Image
+          src="/assets/icon.jpg"
+          alt="icon"
+          width={150}
+          height={150}
+          style={{ borderRadius: "50%", flexShrink: 0 }}
+        />
+        <div>
+          <Text fw={700} size="lg">
+            Raiki Tamura
+          </Text>
+          <Space h="sm" />
+          <Text>Master&apos;s student at Kyoto University</Text>
+        </div>
+      </div>
 
       <Text>
-        I am a master student at Kyoto University.
-        I love learning new things and applying them to real-world problems.
+        I am a master&apos;s student with a focus on compilers, programming
+        language runtimes, and static analysis.
       </Text>
-
-      <Space h="md" />
-
-      <Text fw={700} size="lg">
-        Skills
-      </Text>
-      <List>
-        <List.Item>Experienced: C, C++, Rust, WebAssembly</List.Item>
-        <List.Item>
-          Intermediate: React, TypeScript, JavaScript, Python
-        </List.Item>
-        <List.Item>Tools: Git, GCP, Firebase, Linux, Docker</List.Item>
-      </List>
-
-      <Space h="md" />
-
-      <Text fw={700} size="lg">
-        Research Interests
-      </Text>
-      <List>
-        <List.Item>Compilers, Interpreters, Program analyses</List.Item>
-        <List.Item>WebAssembly, Rust</List.Item>
-      </List>
 
       <Space h="md" />
 
@@ -61,8 +40,8 @@ export default function About() {
           Informatics, Social Informatics Course.
         </List.Item>
         <List.Item>
-          Apr 2021 - Mar 2025: B.E. in Kyoto University, Faculty of
-          Engineering, Department of Informatics.
+          Apr 2021 - Mar 2025: B.E. in Kyoto University, Faculty of Engineering,
+          Department of Informatics.
         </List.Item>
       </List>
 
@@ -72,8 +51,8 @@ export default function About() {
         Work Experience
       </Text>
       <List>
-        <List.Item>Aug 2025 - Present: Woven by Toyota (Intern).</List.Item>
         <List.Item>Dec 2023 - Present: estie, inc (Part-time).</List.Item>
+        <List.Item>Aug 2025 - Oct 2025: Woven by Toyota (Intern).</List.Item>
         <List.Item>Aug 2023 - Sep 2024: Sony (Intern).</List.Item>
         <List.Item>Aug 2022 - Sep 2025: NTT R&D (Intern).</List.Item>
       </List>
@@ -120,16 +99,16 @@ export default function About() {
         <List.Item>
           2025 AsiaLLVM. Wanco: WebAssembly AOT Compiler That Supports Live
           Migration
-          <Link
-            href="https://docs.google.com/presentation/d/1fbAiPh-o_3Xg10fX0Zf-ngISpc_8olzNYTeVDzwqWW8/edit?usp=sharing"
-          >(slides)</Link>
+          <Link href="https://docs.google.com/presentation/d/1fbAiPh-o_3Xg10fX0Zf-ngISpc_8olzNYTeVDzwqWW8/edit?usp=sharing">
+            (slides)
+          </Link>
           .
         </List.Item>
         <List.Item>
           FOSDEM 2024. Unicode Support for GCC Rust
-          <Link
-            href="https://docs.google.com/presentation/d/1mdxOBGrBCozhx3hY729tWGh33bqqgYO0xddpK5ql0qY/edit?usp=drive_link"
-          >(slides)</Link>
+          <Link href="https://docs.google.com/presentation/d/1mdxOBGrBCozhx3hY729tWGh33bqqgYO0xddpK5ql0qY/edit?usp=drive_link">
+            (slides)
+          </Link>
           .
         </List.Item>
       </List>
@@ -137,7 +116,7 @@ export default function About() {
       <Space h="md" />
 
       <Text fw={700} size="lg">
-        OSS
+        OSS Projects
       </Text>
       <List>
         <List.Item>
@@ -178,9 +157,34 @@ export default function About() {
         Awards
       </Text>
       <List>
-        <List.Item>IPSJ Computer Science Research Award for Young Scientists (2025)</List.Item>
+        <List.Item>
+          IPSJ Computer Science Research Award for Young Scientists (2025)
+        </List.Item>
         <List.Item>アプリ甲子園 2018 優勝</List.Item>
         <List.Item>Unityインターハイ 2018 ゴールドアワード</List.Item>
+      </List>
+
+      <Space h="md" />
+
+      <Text fw={700} size="lg">
+        Research Interests
+      </Text>
+      <List>
+        <List.Item>Compilers, Interpreters, Program analyses</List.Item>
+        <List.Item>WebAssembly, Proof-of-Work Blockchains</List.Item>
+      </List>
+
+      <Space h="md" />
+
+      <Text fw={700} size="lg">
+        Skills
+      </Text>
+      <List>
+        <List.Item>Experienced: C, C++, Rust, WebAssembly</List.Item>
+        <List.Item>
+          Intermediate: React, TypeScript, JavaScript, Python
+        </List.Item>
+        <List.Item>Tools: Git, GCP, Firebase, Linux, Docker</List.Item>
       </List>
 
       <Space h="md" />
@@ -189,34 +193,31 @@ export default function About() {
         Others
       </Text>
       <List>
+        <List.Item>2025年度未踏IT人材発掘・育成事業 クリエイター</List.Item>
         <List.Item>
-          2025年度未踏IT人材発掘・育成事業
+          Kyoto University, Faculty of Engineering, Department of Informatics,
+          Operating Systems. TA
         </List.Item>
         <List.Item>
-          Kyoto University, Faculty of Engineering, Department of
-          Informatics, Operating Systems. TA
-        </List.Item>
-        <List.Item>
-          Kyoto University, Faculty of Engineering, Department of
-          Informatics, Computer Architecture. TA
+          Kyoto University, Faculty of Engineering, Department of Informatics,
+          Computer Architecture. TA
         </List.Item>
         <List.Item>セキュリティ・ミニキャンプ熊本2024 チューター</List.Item>
-        <List.Item>
-          Google Summer of Code 2023. (Organization: GCC)
-        </List.Item>
-        <List.Item>
-          Global Cybersecurity Camp 2023 Singapore. Tutor
-        </List.Item>
+        <List.Item>Google Summer of Code 2023. (Organization: GCC)</List.Item>
+        <List.Item>Global Cybersecurity Camp 2023 Singapore. Tutor</List.Item>
         <List.Item>セキュリティ・ミニキャンプ宮崎2023 チューター</List.Item>
-        <List.Item>
-          セキュリティ・キャンプ全国大会 2022. チューター
-        </List.Item>
+        <List.Item>セキュリティ・キャンプ全国大会 2022. チューター</List.Item>
         <List.Item>
           セキュリティ・キャンプ全国大会 2021 脅威解析トラックコース 修了
         </List.Item>
         <List.Item>第8回 科学の甲子園 全国大会 埼玉県代表</List.Item>
         <List.Item>SecHack365 開発駆動コース 修了</List.Item>
       </List>
+
+      <Space h="xl" />
+      <Text size="sm" c="dimmed">
+        Last updated: 2026-02-17
+      </Text>
     </div>
   );
 }
